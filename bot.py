1801a8e14b4b5fd3daa9f94ed3845cb5a702951d
Gustavo Maia <67283753+gustavopmaia@users.noreply.github.com>
2022-11-23 10:45:09 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -36,6 +36,56 @@ p.click()
 time.sleep(4)
 print(p.position())
 
+#defining anticaptcha function
+def acp_api_send_request(driver, message_type, data={}):
+    message = {
+		# this receiver has to be always set as antiCaptchaPlugin
+        'receiver': 'antiCaptchaPlugin',
+        # request type, for example setOptions
+        'type': message_type,
+        # merge with additional data
+        **data
+    }
+    # run JS code in the web page context
+    # preceicely we send a standard window.postMessage method
+    return driver.execute_script("""
+    return window.postMessage({});
+    """.format(json.dumps(message)))
+# Init the chrome options object for connection the extension
+options = webdriver.ChromeOptions()
+# A full path to CRX or ZIP or XPI file which was downloaded earlier 
+options.add_extension('<path to the plugin>')
+# Run the browser (Chrome WebDriver) with passing the full path to the downloaded WebDriver file
+driver = webdriver.Chrome('chromedriver', options=options)
+
+# Go to the empty page for setting the API key through the plugin API request
+driver.get('https://antcpt.com/blank.html')
+
+# Setting up the anti-captcha.com API key 
+# replace YOUR-ANTI-CAPTCHA-API-KEY to your actual API key, which you can get from here:
+# https://anti-captcha.com/clients/settings/apisetup
+acp_api_send_request(
+    driver,
+    'setOptions',
+    {'options': {'antiCaptchaApiKey': '059b13da556382af891625f7472a01d9'}}
+)
+
+# 3 seconds pause
+time.sleep(3)
+driver.get('https://empreenda.digito1.com.br/VotoPopular/Voto/4813')
+# replace the url with the URL you want to scrap
+
+driver.maximize_window()
+# Test input
+driver.find_element_by_css_selector('span[role=checkbox]').send_keys(Keys.ENTER)
+
+# Most important part: we wait upto 120 seconds until the AntiCaptcha plugin indicator with antigate_solver class
+# gets the solved class, which means that the captcha was successfully solved
+WebDriverWait(browser, 120).until(lambda x: x.find_element_by_css_selector('.antigate_solver.solved'))
+
+# Sending form
+driver.find_element_by_css_selector('input[type=submit]').click()
+
 
 # Point(x=756, y=391)
 # Point(x=1142, y=687)
